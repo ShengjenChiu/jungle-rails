7447f217303ef9e42fe641dfa863dd42913a3932
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-08-24 16:56:09 -0600

    Feature: Admin Dashboard - Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @number_of_products = Product.count
+    @number_of_categories = Category.count
+
+    render 'admin/dashboard/show'
   end
 end
