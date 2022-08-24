9be0c286e6abe3eac6414f29f906dadfc5623090
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-08-24 09:41:45 -0600

    fix/money-formatting

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,21 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+
+# puts "inside index def"
+
+render 'index'
+
+
   end
 
   def show
     @product = Product.find params[:id]
+
+    # puts "inside show def"
+
+    render :show
+
   end
 
 end
