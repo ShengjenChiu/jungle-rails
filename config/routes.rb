8a7b1986ea5350eb07ffc9978cbd624081038885
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-08-24 18:35:28 -0600

    generate About Us page and linking to it at the bottom nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
+  
+  get 'about/index', to: 'about#index'
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
